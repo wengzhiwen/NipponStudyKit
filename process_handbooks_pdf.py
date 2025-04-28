36622d6777bcc8aadb827fa1fdf240765f6f6b62
James Weng <wengzhiwen@gmail.com>
2025-04-28 15:44:39 +0000

    更新.gitignore文件，添加buffalo_workspace目录；在process_handbooks_pdf.py中引入新的依赖，重构配置类以支持buffalo项目的创建和处理PDF文件的工作流

--- process_handbooks_pdf.py
+++ process_handbooks_pdf.py
@@ -1,12 +1,17 @@
 import argparse
 from datetime import datetime
+import time
+import glob
 import os
 import sys
+from pathlib import Path
 
 from dotenv import load_dotenv
 from logging_config import setup_logger
+from buffalo import Buffalo, Work, Project
 
-logger = setup_logger(logger_name="md_analysis", log_level="INFO")
+logger = setup_logger(logger_name="ProcessHandbooksPDF", log_level="INFO")
+
 
 class Config:
     """配置类，用于管理所有配置信息（单例模式）"""
@@ -23,17 +28,58 @@ class Config:
             return
 
         load_dotenv()
-        
-        self.base_dir = os.getenv("BASE_DIR", "bufallo_workspace")
-        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        output_dir_basename = os.getenv("OUTPUT_DIR_BASENAME", "pdf_with_md")
-        self.output_dir = f"{output_dir_basename}_{timestamp}"
+
+        try:
+            self.base_dir = Path(os.getenv("BASE_DIR", "bufallo_workspace"))
+            self.base_dir.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            raise ValueError(f"Base directory cannot be created: {self.base_dir}") from e
+
+        try:
+            self.buffalo_template_file = Path(os.getenv("BUFFALO_TEMPLATE_FILE_NAME", "wf_template.yml"))
+            if not self.buffalo_template_file.exists():
+                raise FileNotFoundError(f"Buffalo template file not found: {self.buffalo_template_file}")
+        except Exception as e:
+            raise FileNotFoundError(f"Buffalo template file not found: {self.buffalo_template_file}") from e
 
         self._initialized = True
 
 
 def workflow(pdf_folder):
-    pass
+    config = Config()
+
+    # 获取所有PDF文件
+    pdf_files = glob.glob(os.path.join(pdf_folder, '*.pdf'))
+    total_pdfs = len(pdf_files)
+    created_buffalo_projects = 0
+
+    logger.info(f'找到 {total_pdfs} 个PDF文件')
+
+    for pdf_path in pdf_files:
+        logger.info(f'\n处理 {os.path.basename(pdf_path)}...')
+
+        try:
+            buffalo = Buffalo(base_dir=config.base_dir, template_path=config.buffalo_template_file)
+
+            # 以pdf的basename的前5位加日期时间作为buffalo project的name
+            project_folder_name = os.path.basename(pdf_path)[:5] + datetime.now().strftime('%Y%m%d_%H%M%S')
+            project: Project = buffalo.create_project(project_folder_name)
+
+            project.move_to_project(Path(pdf_path), "handbook.pdf")
+
+            created_buffalo_projects += 1
+
+            logger.info(f'进度: {created_buffalo_projects}/{total_pdfs} 已创建buffalo项目')
+        except Exception as e:
+            logger.error(f'错误: 创建buffalo项目失败，错误信息: {e}')
+            continue
+        finally:
+            # 等待1秒
+            time.sleep(1)
+
+    logger.info('\n处理完成:')
+    logger.info(f'总共创建的buffalo项目数: {created_buffalo_projects}')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='将PDF文件转换为Markdown格式并提取招生信息')
@@ -46,5 +92,5 @@ if __name__ == '__main__':
     if not os.path.isdir(args.dir):
         logger.error(f'错误: {args.dir} 不是一个目录')
         sys.exit(1)
-    
+
     workflow(args.dir)
